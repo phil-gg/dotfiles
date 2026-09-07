6d098930dc2f4ae6a753d77ce7f748cca502f373
phil-gg <45383744+phil-gg@users.noreply.github.com>
2026-09-07 15:46:41 +1000

    Update run_after_02-configure-repos-update-pkgs-Debian.sh

--- HOME/dot_config/scripts/run_after_02-configure-repos-update-pkgs-Debian.sh
+++ HOME/dot_config/scripts/run_after_02-configure-repos-update-pkgs-Debian.sh
@@ -964,7 +964,7 @@ count_install_pkgs=$(printf "%s" "${install_dry_run}" | grep -c '^Inst ')
 if (( apt_exit_status != 0 )) || (( count_install_pkgs > 0 )); then
 echo -e "\n${cyanbold}Run apt install${normal}"
 echo -e "$ sudo DEBIAN_FRONTEND=noninteractive apt install -y ${PACKAGES[*]}\n"
-sudo script -qec "DEBIAN_FRONTEND=noninteractive apt install -y ${PACKAGES[*]}" /dev/null |
+sudo script -qec "DEBIAN_FRONTEND=noninteractive apt install -y ${PACKAGES[*]}" /dev/null < /dev/null |
 grep --color=always -v "is already the newest version"
 fi
 
