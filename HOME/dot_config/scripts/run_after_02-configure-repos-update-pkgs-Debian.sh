822e502eaeb875824baf34fac79c8700c7651beb
phil-gg <45383744+phil-gg@users.noreply.github.com>
2026-09-14 08:19:23 +1000

    Update run_after_02-configure-repos-update-pkgs-Debian.sh

--- HOME/dot_config/scripts/run_after_02-configure-repos-update-pkgs-Debian.sh
+++ HOME/dot_config/scripts/run_after_02-configure-repos-update-pkgs-Debian.sh
@@ -553,7 +553,7 @@ don't exceed release version for testing (forky)
 backports-sloppy\"
 # available components: main contrib non-free-firmware non-free
 # available architectures: amd64 arm64 armel armhf i386 ppc64el riscv64 s390x
-Types: deb
+Types: deb deb-src
 URIs: https://deb.debian.org/debian/
 Suites: trixie trixie-updates trixie-proposed-updates trixie-backports \
 trixie-backports-sloppy
@@ -579,7 +579,7 @@ SID_SOURCES="\
 # available components: main contrib non-free-firmware non-free
 # available architectures: amd64 arm64 armhf i386 loong64 ppc64el riscv64 \
 s390x
-Types: deb deb-src
+Types: deb
 URIs: https://deb.debian.org/debian/
 Suites: sid
 Components: main contrib non-free-firmware non-free
