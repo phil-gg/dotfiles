4f0cc4850a103f4bda89930bc119216a34c1b5f9
phil-gg <45383744+phil-gg@users.noreply.github.com>
2026-09-14 09:21:34 +1000

    Update run_after_02-configure-repos-update-pkgs-Debian.sh

--- HOME/dot_config/scripts/run_after_02-configure-repos-update-pkgs-Debian.sh
+++ HOME/dot_config/scripts/run_after_02-configure-repos-update-pkgs-Debian.sh
@@ -892,7 +892,7 @@ readarray -t PACKAGES < <(printf '%s\n' "${PACKAGES[@]}" | awk 'NF && !seen[$0]+
 
 # Show array
 echo -e "\n${bluebold}PACKAGES list${normal}"
-printf '%s\n' "${PACKAGES[@]}"
+printf '%s\n' "${PACKAGES[@]}" | column -x -c $(tput cols)
 
 # Only run all this if you can markauto with aptitude
 if command -v aptitude &> /dev/null; then
@@ -997,7 +997,7 @@ if [[ -n "$pkgwarning" ]]; then
 # Keep the packages you are warning about, marked for installation
 mapfile -t -O "${#PACKAGES[@]}" PACKAGES <<< "${pkgwarning}"
 echo -e "\n${redbold}WARNING: Unexpected Debian packages installed${normal}"
-echo -e "${pkgwarning}"
+echo -e "${pkgwarning}" | column -x -c $(tput cols)
 fi
 
 # Make unmarked PACKAGES apt-mark manual in apt
